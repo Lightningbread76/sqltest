e24c389af606c9d290d9b7e81b63d9eb34359717
Lightningbread76 <67837759+Lightningbread76@users.noreply.github.com>
2022-06-01 23:17:34 -0400

    set up working front end and backend

--- server/server.js
+++ server/server.js
@@ -1,13 +1,47 @@
-const express = require("express");
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT ? parseInt(process.env.PORT) : 5000;
 
-const app = express();
+var indexRouter = require('./routes/index');
+//var formRouter = require('./routes/form');
 
-app.get("/api", (req, res) =>{
-  res.json({"users": ["userOne", "userTwo" ,"userThree"] })
-})
+var app = express();
+
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+app.use('/', indexRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
+
+module.exports = app;
+
